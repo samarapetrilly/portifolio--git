576e48f41521873d57c2248f1310500456a06a1d
samara <saharruda2328@gmail.com>
2026-04-08 15:44:43 -0300

    feat(ex02): adiciona entrada de nome do usuário

--- exercicios/ex02-branches/ola_mundo.cpp
+++ exercicios/ex02-branches/ola_mundo.cpp
@@ -4,9 +4,14 @@ using namespace std;
 int main(){
     system("cls");
 
+    string nome;
+
     cout << "Olá Mundo!\n";
     cout << "Seja bem-vindo(a) ao programa\n";
     cout << "Tudo bem com voce? :)";
+
+    cout << "Qual o seu nome? ";
+    cin >> nome;
 
     return 0;
 }
