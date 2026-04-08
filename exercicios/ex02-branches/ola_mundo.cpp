8d49e92cf464a5ed154c7b9cc88b3b0110f182f4
samara <saharruda2328@gmail.com>
2026-04-08 16:10:57 -0300

    docs(ex02): adiciona comentarios explicativos ao codigo

--- exercicios/ex02-branches/ola_mundo.cpp
+++ exercicios/ex02-branches/ola_mundo.cpp
@@ -4,18 +4,24 @@ using namespace std;
 int main(){
     system("cls");
 
+    // Declaração de variavel
     string nome;
 
+    // Mensagem inicial
     cout << "Olá Mundo!\n";
+
+    //Saudação Inicial
     cout << "Seja bem-vindo(a) ao programa\n";
     cout << "Tudo bem com voce? :)";
 
+    // Entrada do nome do usuario
     cout << "Qual o seu nome? ";
     cin >> nome;
 
+    // Saudação personalizada
     cout << "Que nome bonito ;)\n";
     cout << "Olá, " << nome << "!";
-    cout << "Prazer em te conhecer.";
+    cout << "Prazeremteconhecer.";
 
     return 0;
 }
