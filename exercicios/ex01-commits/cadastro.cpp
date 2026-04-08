32d2efd56a06c8c40f4b43bd85571ffad16ec61e
samara <saharruda2328@gmail.com>
2026-04-08 15:01:17 -0300

    feat(ex01): implementa entrada de gênero do usuário

--- exercicios/ex01-commits/cadastro.cpp
+++ exercicios/ex01-commits/cadastro.cpp
@@ -12,30 +12,41 @@ int main()
     char nome[20] = "";
     char nacionalidade[15] = "";
     string endereco = "";
+    char genero[12] = "";
 
     // Inicio do programa
     
     cout << "----CADASTRO----\n";
+    cout << "----------------";
 
-    cout << "Informe seu Nome: ";
+    cout << "Informe seu Nome:";
     cin>>nome;
+    cout << "----------------";
 
-    cout<<"Informe sua Idade: ";
+    cout<<"Informe sua Idade:";
     cin>>idade;
+    cout << "----------------";
 
-    cout<<"Informe sua Nacionalidade: ";
+    cout<<"Informe sua Nacionalidade:";
     cin>>nacionalidade;
+    cout << "----------------";
 
     fflush(stdin);
-    cout<<"Informe seu Endereco: ";
+    cout<<"Informe seu Endereco:";
     getline(cin,endereco);
+    cout << "----------------";
+
+    cout<<"Informe seu Genero:";
+    cin>>genero;
+    cout << "----------------";
 
     // Impressão de dados no terminal
     cout<<"\n\n-------Imprimindo Dados Informados-------";
-    cout<<"\nNome: " << nome <<".";
-    cout<<"\nIdade: " << idade <<".";
-    cout<<"\nNacionalidade: " << nacionalidade <<".";
-    cout<<"\nEndereco: " << endereco << ".";
+    cout<<"\nNome:" << nome;
+    cout<<"\nIdade: " << idade;
+    cout<<"\nNacionalidade: " << nacionalidade;
+    cout<<"\nEndereco: " << endereco;
+    cout<<"\nGenero: " << genero;
 
     return 0;
 }
