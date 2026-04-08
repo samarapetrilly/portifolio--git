9e4014d9503abce900a90bdbdacea69faee8f204
samara <saharruda2328@gmail.com>
2026-04-08 13:50:55 -0300

    feat(jogo-adivinhacao): adiciona a estrutura base do jogo

---
+++ projeto/jogo-adivinhação/jogo-adivinhacao.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    system("cls");
+
+    cout<<"------------------\n\n";
+    cout<<"| Inicio do Jogo |\n\n";
+    cout<<"Adivinhe o numero entre 1 e 15!\n\n";
+
+    return 0;
+}
