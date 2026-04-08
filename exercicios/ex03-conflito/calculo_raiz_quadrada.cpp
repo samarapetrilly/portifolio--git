9f287e9fcf096526cc70d25350288c4c16c8bc84
samara <saharruda2328@gmail.com>
2026-04-08 19:24:49 -0300

    fix(ex03): corrige formatação e adiciona comentários

--- exercicios/ex03-conflito/calculo_raiz_quadrada.cpp
+++ exercicios/ex03-conflito/calculo_raiz_quadrada.cpp
@@ -7,22 +7,29 @@ int main()
 {
     system("cls");
 
+    // Declaração das variaveis
     double x = 0;
     char opc;
     
+    // Ponto de reinicio do programa
     inicio:
 
+    // Mensagem inicial
     cout << "Boas Vindas!\n";
     cout << "|-----Calcule a Raiz Quadrada-----|\n";
 
+    // Entrada do usuário
     cout << "Informe o valor da raiz: ";
     cin >> x;
 
+    // Calculo da raiz
     double raiz_de_x = sqrt(x);
     
+    // Impressão no terminal
     cout<<"Raiz quadrada = " << x;
     
-    cout << "Desejacalcular.outra raiz? (s/n): ";
+    // Reiniciar o programa
+    cout << "Deseja calcular novamente? (s/n): ";
     cin >> opc;
 
     if(opc == 's' or opc == 'S'){
